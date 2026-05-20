00f88948ce3174397e9b5e4d74a46782683c73c2
Sumanth Roy <sumanthbanisetti2354@gmail.com>
2026-05-20 16:30:38 +0530

    Hardcode backend URL in frontend and allow dynamic CORS for seamless deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: CLIENT_URL,
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
